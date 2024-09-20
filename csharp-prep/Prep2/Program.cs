7f1633c003989153020cb42ddcf1058d3e3d5577
Maddie :) <maddiemunch123@gmail.com>
2024-09-20 11:08:02 -0600

    Completed Prep 2 Letter Grade assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        string letterGrade = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (percentage % 10 >= 7) 
+        {
+            if (letterGrade != "A" && letterGrade != "F")
+            {
+                letterGrade += "+";
+            }
+        }
+        else if (percentage % 10 < 3)
+        {
+            if (letterGrade != "F")
+            {
+                letterGrade += "-";
+            }
+        }
+
+        Console.WriteLine(letterGrade);
+        Console.WriteLine(percentage >= 70 ? "Congratulations! You passed the class!" : "Sorry, your grade was too low. You can do better next time!");
+
     }
 }
