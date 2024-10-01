c402996af47210c311be716240ea353db70a15c6
Maddie :) <maddiemunch123@gmail.com>
2024-10-01 15:02:48 -0600

    Resume and Job assignment!

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
+//using Job;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Barbietown News Channel";
+        job1._jobTitle = "Reporter";
+        job1._startYear = 2017;
+        job1._endYear = 2024;
+        
+        Job job2 = new Job();
+        job2._company = "BarbieTown Palace News";
+        job2._jobTitle = "News from the Palace Man";
+        job2._startYear = 2017;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Bob Thompson";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("Enter your favorite color: ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Here is your favorite color: {color}");
+
+        List<string> animals = new List<string>();
+        animals.Add("chicken");
+        animals.Add("goat");
+        animals.Add("sheep");
+        animals.Add("cow");
+
+        Console.WriteLine("Animals:");
+        foreach (string animal in animals)
+        {
+            Console.WriteLine($" * {animal}");
+        }
     }
 }
