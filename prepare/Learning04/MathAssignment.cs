5d516a6b00849930c652967dcb51ffc044deef4d
Maddie :) <maddiemunch123@gmail.com>
2024-10-28 17:28:42 -0600

    Completed Assignment assignment :)

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,17 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string name, string topic, string textbookSelection, string problems) : base(name, topic)
+    {
+        _textbookSection = textbookSelection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"{_textbookSection} {_problems}";
+    }
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Bob Thompson", "Mathss");
+        MathAssignment mats = new MathAssignment("Bob THompson", "Mathies", "Section 2", "Problems 2-222");
+        Console.WriteLine(mats.GetSummary());
+        Console.WriteLine(mats.GetHomeworkList());
+        WritingAssignment paper = new WritingAssignment("Bob Thompson", "English Paper", "History of Reporting");
+        Console.WriteLine(paper.GetSummary());
+        Console.WriteLine(paper.GetWritingInformation());
     }
 }
